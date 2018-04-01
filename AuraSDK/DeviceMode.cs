73b1a6b58fe651f18297e8741472f49af1cdbb5a
Nicolas Gnyra <nicolas@gnyra.com>
2018-04-01 15:51:44 -0400

    Add explanations

--- AuraSDK/DeviceMode.cs
+++ AuraSDK/DeviceMode.cs
@@ -2,7 +2,14 @@
 {
     public enum DeviceMode : int
     {
+        /// <summary>
+        /// Default mode. The device does whatever it wants.
+        /// </summary>
         EC = 0,
+
+        /// <summary>
+        /// Software control mode. Device LEDs are controlled exclusively via software.
+        /// </summary>
         Software = 1
     }
 }
